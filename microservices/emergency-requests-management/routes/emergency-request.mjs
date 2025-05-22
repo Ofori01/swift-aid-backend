ed15e3bb96a7fbb3902c46f5d161cce4e8aba23b
Ofori01 <oforidarkwah7@gnail.com>
2025-05-22 09:47:19 +0000

    fixed user signup and login errors

--- microservices/emergency-requests-management/routes/emergency-request.mjs
+++ microservices/emergency-requests-management/routes/emergency-request.mjs
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import { imageHandler } from "../../../utils/images/imageHandler.mjs";
+import {singleImageHandler } from "../../../utils/images/imageHandler.mjs";
 import { createEmergencyRequest } from "../controllers/create-emergency-request.mjs";
 import { getAiRecommendations } from "../controllers/get-recommendations.mjs";
 
@@ -14,7 +14,7 @@ const emergencyRequestRouter = Router();
 
 
 
-emergencyRequestRouter.post('/create', upload.single('image'), imageHandler, createEmergencyRequest,getAiRecommendations)
+emergencyRequestRouter.post('/create', upload.single('image'), singleImageHandler, createEmergencyRequest,getAiRecommendations)
 
 
 
