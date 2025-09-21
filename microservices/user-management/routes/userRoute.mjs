baa21cb63042f899506e269c38a175523d43e0ab
Ofori01 <oforidarkwah7@gnail.com>
2025-09-21 12:27:37 +0000

    added user getting emergencies

--- microservices/user-management/routes/userRoute.mjs
+++ microservices/user-management/routes/userRoute.mjs
@@ -1,23 +1,28 @@
-import express from 'express'
-import { getUser, getUsers, updateUser } from '../controllers/userController.mjs'
-import multer from 'multer'
-import { userLogin, userSignup } from '../controllers/userAuthControllers.mjs'
+import express from "express";
+import {
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/userController.mjs";
+import { getUserEmergencies } from "../controllers/userEmergencyController.mjs";
+import multer from "multer";
+import { userLogin, userSignup } from "../controllers/userAuthControllers.mjs";
 
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const uploadConfig = upload.fields([
-    {name: 'ghana_card_image_front', maxCount: 1},
-    {name: 'ghana_card_image_back', maxCount: 1}
-])
+  { name: "ghana_card_image_front", maxCount: 1 },
+  { name: "ghana_card_image_back", maxCount: 1 },
+]);
 
+const userRouter = express.Router();
 
-const userRouter = express.Router()
+userRouter.get("/user/:id", getUser);
+userRouter.get("/users", getUsers);
+userRouter.get("/user/:id/emergencies", getUserEmergencies);
+userRouter.put("/user/:id", updateUser);
+userRouter.post("/user/signup", uploadConfig, userSignup);
+userRouter.post("/user/login", userLogin);
 
-userRouter.get('/user/:id', getUser)
-userRouter.get('/users', getUsers)
-userRouter.put('/user/:id', updateUser)
-userRouter.post('/user/signup',uploadConfig,userSignup)
-userRouter.post('/user/login', userLogin)
-
-export default userRouter
+export default userRouter;
